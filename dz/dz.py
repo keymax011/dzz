d78e1fe26c4846fac18927bfe7c976752d4aeea1
keymax011 <87239064+keymax011@users.noreply.github.com>
2021-08-02 13:13:37 +0300

    Update dz.py

--- dz/dz.py
+++ dz/dz.py
@@ -1,49 +1,25 @@
-import random
-l = []
-for i in range(10):
-    l.append(random.randint(0, 100))
-print(l)
-print(list(set(l)))
+# def calculations(action, n, m):
+#     if action == sum:
+#         print(n+m)
+#         return action
+#     if action == min:
+#         print(n-m)
+#         return action
+# print(calculations(min, 8, 7))
 
-# import random
-# l1 = []
-# for i in range(10):
-#     l1.append(random.randint(0, 100))
-# print(l1)
-# l2 = []
-# for i in l1:
-#     l2.append(i)
-# print(l2)
+# def listsum(numList):
+#     theSum = 0
+#     for i in numList:
+#         theSum = theSum + i
+#     return theSum
+# print(listsum([1,3,5,7,9]))
 
-# import random
-# l1 = []
-# l2 = []
-# l3 = []
-# for i in range(10):
-#     l1.append(random.randint(0, 100))
-# print(l1)
-# for i in range(10):
-#     l2.append(random.randint(0, 100))
-# print(l2)
-# for i in l1:
-#     if i in l2:
-#         l3.append(i)
-# print(l3)
+def arg(*args):
+    print(args)
+    return args
+arg(7, 8, "hello")
 
-# dic1={1:10, 2:20}
-# dic2={3:30, 4:40}
-# dic3={5:50,6:60}
-# dic4 = {}
-# for d in (dic1, dic2, dic3): dic4.update(d)
-# print(dic4)
-
-# import random
-# d = {}
-# x = 0
-# for x in range(0, 1):
-#     x = random.randint(0, 15)
-# print(x)
-# y = x*x
-# print(y)
-# d = {x: y}
-# print(d)
+# def showEmployee(name, salary = 9000):
+#     print(name, salary)
+#     return
+# print(showEmployee("Kkkkk", 5000))
