2033b2b929a85e8aa742e119ff51c0f8a7126e65
keymax011 <87239064+keymax011@users.noreply.github.com>
2021-09-06 16:32:00 +0300

    Update dz.py

--- dz/dz.py
+++ dz/dz.py
@@ -1,94 +1,44 @@
-class Homework:
-    def __init__(self, name, description, complexity, status):
-        self.name = name
-        self.description = description
-        self.complexity = complexity
-        self.status = status
-        self.grade = 0
-
-    def __str__(self):
-        return f"{self.name}, {self.description}, {self.complexity}, {self.status}"
-
-
-class Student:
-    def __init__(self, name, age, subscribed_course):
+class Table:
+    def __init__(self, name, age, avarage_grade, course):
         self.name = name
         self.age = age
-        self.avarage_grade = 0
-        self.subscribed_course = subscribed_course
+        self.avarage_grade = avarage_grade
+        self.course = course
         self.homeworks = []
 
     def __str__(self):
-        ret = f"{self.name}, age: {self.age}, grade: {self.avarage_grade}, course: {self.subscribed_course}\n"
+        ret = f"{self.name}, age: {self.age}, avarage grade: {self.avarage_grade}, course: {self.course}\n"
         ret = ret + "Homeworks:\n"
         for h in self.homeworks:
             ret = ret + "\t" + str(h) + "\n"
         return ret
 
-    def add_homework(self, homework):
-        self.homeworks.append(homework)
+class Homeworks:
+    def __init__(self, title, theme, course):
+        self.title = title
+        self.theme = theme
+        self.course = course
 
-    def homework_status(self, status):
-        ret_status = False
-        if status == "+":
-            ret_status = True
-        for i in students:
-            for h in i.homeworks:
-                if ret_status == True:
-                    h.status = True
+    def __str__(self):
+        return f"{self.title}, {self.theme}, {self.course}"
 
+homework = [Homeworks("Homework 1", "Variables","Python"),
+            Homeworks("Homework 2", "Lists", "Python")]
 
-# Helpers functions
+def add_homework(homework):
+    SH = []
+    for i in homework:
+        SH.append(str(i))
+    for h in students:
+        h.homeworks.append(SH)
+
 def show_students(students):
-    for s in students:
-        print(s)
+    for i in students:
+        print(i)
 
-def sort_by_age(students):
-    return sorted(students, key=lambda s: s.age)
+students = [Table("Richard", 14, 78, "Python"),
+            Table("Nikolai", 17, 34, "Python"),
+            Table("Maria", 16, 97, "Python")]
 
-def sort_by_grade(students):
-    return sorted(students, key=lambda s: s.avarage_grade, reverse=True)
-
-class Table:
-    def __init__(self, name, age, subscribed_course):
-        self.name = name
-        self.age = age
-        self.avarage_grade = 0
-        self.subscribed_course = subscribed_course
-        self.homeworks = []
-
-    def show_studentss(studentss):
-        for s in studentss:
-            print(s)
-
-    def add_homeworks(self, homework):
-        for i in homework:
-            self.homeworks.append(i)
-
-students = [
-    Student("Student 1", 12, "Python")
-    , Student("Student 2", 16, "Python")
-    , Student("Student 3", 15, "Python")
-    , Student("Student 4", 14, "Python")
-    , Student("Student 5", 10, "Python")
-]
-
-studentss = [
-    Student("Student 1", 12, "Python")
-    , Student("Student 2", 16, "Python")
-    , Student("Student 3", 15, "Python")
-    , Student("Student 4", 14, "Python")
-    , Student("Student 5", 10, "Python")
-    , Student("Student 6", 14, "Python")
-    , Student("Student 7", 11, "Python")
-]
-
-homeworks = [
-    Homework ("Homework 1", "Description 1", 2, False)
-    ,Homework ("Homework 2", "Description 2", 5, False)
-]
-
-
-
-print("Initial")
-show_students(studentss)
+add_homework(homework)
+show_students(students)
