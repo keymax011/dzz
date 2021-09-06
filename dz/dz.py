f733f0b84357fb983a50e944a01494dd0dce9564
keymax011 <87239064+keymax011@users.noreply.github.com>
2021-09-06 17:26:15 +0300

    Update dz.py

--- dz/dz.py
+++ dz/dz.py
@@ -1,4 +1,4 @@
-class Table:
+class Student:
     def __init__(self, name, age, avarage_grade, course):
         self.name = name
         self.age = age
@@ -8,6 +8,20 @@ class Table:
 
     def __str__(self):
         ret = f"{self.name}, age: {self.age}, avarage grade: {self.avarage_grade}, course: {self.course}\n"
+        ret = ret + "Homeworks:\n"
+        for h in self.homeworks:
+            ret = ret + "\t" + str(h) + "\n"
+        return ret
+
+class Table:
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+        self.homeworks = []
+
+    def __str__(self):
+        ret = f"{self.name}, age: {self.age}, course: {self.course}\n"
         ret = ret + "Homeworks:\n"
         for h in self.homeworks:
             ret = ret + "\t" + str(h) + "\n"
@@ -25,20 +39,32 @@ class Homeworks:
 homework = [Homeworks("Homework 1", "Variables","Python"),
             Homeworks("Homework 2", "Lists", "Python")]
 
-def add_homework(homework):
+def add_homework(homework, student):
     SH = []
     for i in homework:
         SH.append(str(i))
-    for h in students:
+    for h in student:
         h.homeworks.append(SH)
 
 def show_students(students):
     for i in students:
         print(i)
 
-students = [Table("Richard", 14, 78, "Python"),
-            Table("Nikolai", 17, 34, "Python"),
-            Table("Maria", 16, 97, "Python")]
+def show_table(table):
+    for i in table:
+        print(i)
 
-add_homework(homework)
+students = [Student("Richard", 14, 78, "Python"),
+            Student("Nikolai", 17, 34, "Python"),
+            Student("Maria", 16, 97, "Python")]
+
+table_students = [Table("Richard", 14, "Python"),
+            Table("Nikolai", 17, "Python"),
+            Table("Maria", 16, "Python")]
+
+add_homework(homework, students)
+add_homework(homework, table_students)
+print("Students", "\n")
 show_students(students)
+print("Table", "\n")
+show_table(table_students)
